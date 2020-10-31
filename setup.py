d4cb38c8a5e91b88052d0190db7e94c42eb158e1
Anton K <kakkazak@gmail.com>
2020-10-31 15:27:36 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='async_server',
+    version='0.0.1',
+    author='Anton Kazak',
+    author_email='anton_kazak@ukr.net',
+    description='Async server',
+    install_requires=[]
+)
