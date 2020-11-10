1ab5d5e6ca52621e63c017f83acc2c81932eb05c
Anton K <kakkazak@gmail.com>
2020-11-10 23:47:19 +0200

    Run server

--- async_server/main.py
+++ async_server/main.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from async_server.server import Server
 
 
 def run():
     print(f'Time {datetime.utcnow()}')
+    server = Server()
+    server.run()
 
 
 if __name__ == '__main__':
