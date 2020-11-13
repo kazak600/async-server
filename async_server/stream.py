d9b3b8a237d36e9d2f74c1f38f8983193e3640b2
Anton K <kakkazak@gmail.com>
2020-11-13 23:06:06 +0200

    Async stream chunks

--- async_server/stream.py
+++ async_server/stream.py
@@ -1,13 +1,14 @@
 import asyncio
 import collections
 
-from typing import Generic, Callable, Awaitable, TypeVar, Optional
+from typing import Generic, Callable, Awaitable, TypeVar, Optional, Tuple
 
 
 _Type = TypeVar("_Type")
 
 
 class AsyncStreamIterator(Generic[_Type]):
+
     def __init__(self, read_func: Callable[[], Awaitable[_Type]]) -> None:
         self.read_func = read_func
 
@@ -24,7 +25,22 @@ class AsyncStreamIterator(Generic[_Type]):
         return rv
 
 
-class StreamReader(AsyncStreamReaderMixin):
+class ChunkTupleAsyncStreamIterator:
+
+    def __init__(self, stream: "StreamReader") -> None:
+        self._stream = stream
+
+    def __aiter__(self) -> "ChunkTupleAsyncStreamIterator":
+        return self
+
+    async def __anext__(self) -> Tuple[bytes, bool]:
+        rv = await self._stream.readchunk()
+        if rv == (b"", False):
+            raise StopAsyncIteration
+        return rv
+
+
+class StreamReader:
 
     def __init__(
         self,
@@ -37,8 +53,10 @@ class StreamReader(AsyncStreamReaderMixin):
         self._protocol = protocol
         self._low_water = limit
         self._high_water = limit * 2
+
         if loop is None:
             loop = asyncio.get_event_loop()
+
         self._loop = loop
         self._size = 0
         self._cursor = 0
@@ -51,3 +69,27 @@ class StreamReader(AsyncStreamReaderMixin):
         self._exception = None  # type: Optional[BaseException]
         self._timer = timer
         self._eof_callbacks = []  # type: List[Callable[[], None]]
+
+    def __aiter__(self) -> AsyncStreamIterator[bytes]:
+        return AsyncStreamIterator(self.readline)  # type: ignore
+
+    def iter_chunked(self, n: int) -> AsyncStreamIterator[bytes]:
+        """Returns an asynchronous iterator that yields chunks of size n.
+        Python-3.5 available for Python 3.5+ only
+        """
+        return AsyncStreamIterator(lambda: self.read(n))  # type: ignore
+
+    def iter_any(self) -> AsyncStreamIterator[bytes]:
+        """Returns an asynchronous iterator that yields all the available
+        data as soon as it is received
+        Python-3.5 available for Python 3.5+ only
+        """
+        return AsyncStreamIterator(self.readany)  # type: ignore
+
+    def iter_chunks(self) -> ChunkTupleAsyncStreamIterator:
+        """Returns an asynchronous iterator that yields chunks of data
+        as they are received by the server. The yielded objects are tuples
+        of (bytes, bool) as returned by the StreamReader.readchunk method.
+        Python-3.5 available for Python 3.5+ only
+        """
+        return ChunkTupleAsyncStreamIterator(self)  # type: ignore
