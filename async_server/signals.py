de06cec87feff8b65968f08dd5e95e76dcf11b84
Anton K <kakkazak@gmail.com>
2020-11-19 11:24:28 +0200

    move Signal

---
+++ async_server/signals.py
@@ -0,0 +1,12 @@
+class Signal(list):
+
+    def __init__(self, owner):
+        super(Signal, self).__init__()
+        self.owner = owner
+
+    async def send(self, *args, **kwargs):
+        """
+        Sends data to all registered receivers.
+        """
+        for receiver in self:
+            await receiver(*args, **kwargs)
