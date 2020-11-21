6eb693a74adc60bcd7bc243e8cbced8ebd231fc3
Anton K <kakkazak@gmail.com>
2020-11-21 11:29:57 +0200

    cancel tasks

--- async_server/web.py
+++ async_server/web.py
@@ -1,3 +1,29 @@
+def _cancel_tasks(
+    to_cancel: Set["asyncio.Task[Any]"], loop: asyncio.AbstractEventLoop
+) -> None:
+    if not to_cancel:
+        return
+
+    for task in to_cancel:
+        task.cancel()
+
+    loop.run_until_complete(
+        asyncio.gather(*to_cancel, loop=loop, return_exceptions=True)
+    )
+
+    for task in to_cancel:
+        if task.cancelled():
+            continue
+        if task.exception() is not None:
+            loop.call_exception_handler(
+                {
+                    "message": "unhandled exception during asyncio.run() shutdown",
+                    "exception": task.exception(),
+                    "task": task,
+                }
+            )
+
+
 def run_app(
     app: Union[Application, Awaitable[Application]],
     *,
