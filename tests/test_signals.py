f3960f24d1cb79487f0e896d4d924ba445ec896d
Anton K <kakkazak@gmail.com>
2020-11-22 17:46:11 +0200

    test signals

---
+++ tests/test_signals.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import Mock
+from async_server.signals import Signal
+from async_server.web_app import Application
+
+
+class MainTestCase(unittest.TestCase):
+
+    async def test_function_signal_dispatch(self) -> None:
+        app = Application()
+        signal = Signal(app)
+        kwargs = {'foo': 1, 'bar': 2}
+
+        callback_mock = Mock()
+
+        async def callback(**kwargs):
+            callback_mock(**kwargs)
+
+        signal.append(callback)
+        signal.freeze()
+
+        await signal.send(**kwargs)
+        callback_mock.assert_called_once_with(**kwargs)
